2bbad22d4f2c15d0073908eded68fca187537089
s4xack <sergmiron47@gmail.com>
2019-09-29 23:56:04 +0300

    Anti quick-sort added

---
+++ AlgorithmsAndStructures/SortingAlgorithms/AntiQuickSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AlgorithmsAndStructures.SortingAlgorithms
+{
+    class AntiQuickSort
+    {
+        private static void Swap(ref int firstElement, ref int secondElement)
+        {
+            int swapHelper = firstElement;
+            firstElement = secondElement;
+            secondElement = swapHelper;
+        }
+
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] antiArray = Enumerable.Range(1, n).ToArray();
+
+            for (int i = 2; i < n; i++)
+            {
+                Swap(ref antiArray[i], ref antiArray[i / 2]);
+            }
+
+            Console.WriteLine(String.Join(" ", antiArray));
+        }
+    }
+}
