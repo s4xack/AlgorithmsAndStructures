a66014c5b9462ee275ee0e7a22a7037b341d3f32
s4xack <sergmiron47@gmail.com>
2019-10-21 22:44:25 +0300

    Postfix notation added

---
+++ AlgorithmsAndStructures/DataStructures/PostfisNotation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AlgorithmsAndStructures.DataStructures
+{
+    class PostfisNotation
+    {
+        class Stack
+        {
+            private int[] data;
+            private int size;
+
+            public Stack()
+            {
+                data = new int[1000000];
+                size = 0;
+            }
+
+            public int Size()
+            {
+                return size;
+            }
+
+            public void Add(int element)
+            {
+                data[size++] = element;
+            }
+
+            public int Top()
+            {
+                return data[size - 1];
+            }
+
+            public int Pop()
+            {
+                return data[--size];
+            }
+        }
+
+        public static void Main()
+        {
+            string[] sequence = File.ReadAllText("postfix.in").Split();
+            //string[] sequence = Console.ReadLine().Split();
+            Stack stack = new Stack();
+
+            for (int i = 0; i < sequence.Length; ++i)
+            {
+                string element = sequence[i];
+                if (element == "")
+                    continue;
+                int a, b;
+                switch (element)
+                {
+                    case "+":
+                        b = stack.Pop();
+                        a = stack.Pop();
+                        stack.Add(a + b);
+                        break;
+                    case "-":
+                        b = stack.Pop();
+                        a = stack.Pop();
+                        stack.Add(a - b);
+                        break;
+                    case "*": 
+                        b = stack.Pop();
+                        a = stack.Pop();
+                        stack.Add(a * b);
+                        break;
+                    default:
+                        stack.Add(int.Parse(element));
+                        break;
+                }
+            }
+
+            int result = stack.Top();
+            File.WriteAllText("postfix.out", result.ToString());
+            //Console.WriteLine(result.ToString());
+        }
+    }
+}
